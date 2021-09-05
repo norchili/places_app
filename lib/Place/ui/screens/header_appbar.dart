b8834481caa7b617b8daca4278a7862ebb97cc6b
Norberto Aguilar Ramirez <norchilipeper@hotmail.com>
2021-09-05 17:02:30 -0600

    Inicio Curso Avanzado Flutter

---
+++ lib/Place/ui/screens/header_appbar.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import '../../../gradient_back.dart';
+import '../widgets/card_image_list.dart';
+
+class HeaderAppBar extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Stack(
+      children: <Widget>[GradientBack("Bienvenido"), CardImageList()],
+    );
+  }
+}
